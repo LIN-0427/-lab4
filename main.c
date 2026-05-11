4dcacf888762b462b091d0309763f48b8677e987
LIN-0427 <2644353823@qq.com>
2026-05-11 22:52:22 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main() {
+    printf("Hello Git Learning!\n");
+    print_message("Welcome to Git practice");
+    printf("R3 experiment: checking git diff\n");
+    printf("Second modification for practice\n");
+    printf("Third modification - R4 practice\n");
+    return 0;
+}
