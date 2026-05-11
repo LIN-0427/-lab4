121fdcbf0586206a6800d0c378f1f1b9214db35c
LIN-0427 <2644353823@qq.com>
2026-05-11 22:38:18 +0800

    Create example.h

---
+++ example.h
@@ -0,0 +1,15 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+#define MAX_SIZE 100
+#define VERSION "1.0"
+
+typedef struct {
+    int id;
+    char name[50];
+} Person;
+
+void init_person(Person *p, int id, const char *name);
+void print_person(const Person *p);
+
+#endif
