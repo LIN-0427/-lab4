4c5ad476ce3edac1d10c767db71c4888dc4adb12
LIN-0427 <2644353823@qq.com>
2026-05-12 18:49:17 +0800

    B3

--- example.h
+++ example.h
@@ -8,8 +8,11 @@ typedef struct {
     int id;
     char name[50];
 } Person;
-
+s1d54cs41c5fscfsw1v
 void init_person(Person *p, int id, const char *name);
 void print_person(const Person *p);
 
 #endif
+sCFsVDwFCwsCswsCsCs
+111cc223
+
