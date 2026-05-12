d1eaf83eb0b8dab3cad7d66188ac2d0a510d9725
LIN-0427 <2644353823@qq.com>
2026-05-12 18:50:17 +0800

    B3

--- main.c
+++ main.c
@@ -4,8 +4,5 @@
 int main() {
     printf("Hello Git Learning!\n");
     print_message("Welcome to Git practice");
-    printf("R3 experiment: checking git diff\n");
-    printf("Second modification for practice\n");
-    printf("Third modification - R4 practice\n");
-    return 0;
+
 }
